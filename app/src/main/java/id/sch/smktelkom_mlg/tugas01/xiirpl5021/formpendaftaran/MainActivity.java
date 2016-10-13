85ed8c9659a90f5d581b130b5bef870b038f2c7f
KurniaNovitasari <kurnia_dardiri_23rpl@student.smktelkom-mlg.sch.id>
2016-10-13 16:35:32 +0700

    pembuatan layout

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl5021/formpendaftaran/MainActivity.java
@@ -0,0 +1,79 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl5021.formpendaftaran;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    RadioGroup rgJK;
+    CheckBox cbPbb, cbPp, cbLk, cbSb;
+    Spinner spKelas;
+    Button bOk;
+    TextView tvHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        rgJK = (RadioGroup) findViewById(R.id.radioGroupJK);
+        cbPbb = (CheckBox) findViewById(R.id.checkBoxPbb);
+        cbPp = (CheckBox) findViewById(R.id.checkBoxPpaskib);
+        cbLk = (CheckBox) findViewById(R.id.checkBoxLK);
+        cbSb = (CheckBox) findViewById(R.id.checkBoxSbendera);
+        spKelas = (Spinner) findViewById(R.id.spinnerKelas);
+
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+                tvHasil.setText("nama : " + nama);
+            }
+
+        });
+
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+        }
+
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        if (nama.isEmpty()) {
+            etNama.setError("Nama anda belum diisi");
+            valid = true;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = true;
+        } else {
+            etNama.setError(null);
+        }
+        return valid;
+    }
+
+}
+
+
